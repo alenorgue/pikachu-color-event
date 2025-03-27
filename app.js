9e153f41668cefce4d36f343ceb7f8e294ee9863
alenorgue <alenoriegag@gmail.com>
2025-03-27 21:33:04 +0100

    finished

--- app.js
+++ app.js
@@ -2,12 +2,30 @@
 
 // NO MODIFICAR (salvo para conseguir el color sangría)
 
-document.querySelector('#first').addEventListener('click', updateBgColor);
-document.querySelector('#second').addEventListener('click', updateBgColor);
-document.querySelector('#third').addEventListener('click', updateBgColor);
+//document.querySelector('#first').addEventListener('click', updateBgColor);
+//document.querySelector('#second').addEventListener('click', updateBgColor);
+//document.querySelector('#third').addEventListener('click', updateBgColor);
 
 // MODIFICAR A PARTIR DE AQUÍ 
+//document.querySelector('#fourth').addEventListener('click', updateBgColor);
 
 function updateBgColor(event) {
+    let pikachu = document.querySelector('img');
+    let clickedElement = event.target;
+    let color = clickedElement.dataset.color;
+
+    if (!color) {
+        color = window.getComputedStyle(clickedElement).backgroundColor;
+    }
+
+    if (color) {
+        pikachu.style.backgroundColor = color;
+    }
 
 }
+let colorBoxes = document.getElementById('colors')
+
+colorBoxes.addEventListener("click", function (event) {
+  updateBgColor(event)
+});
+      
